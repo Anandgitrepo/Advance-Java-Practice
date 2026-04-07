1915813ac1f6a178081468e8f5816a187c40cd07
Anandgitrepo <aanandchandra5544@gmail.com>
2026-04-07 10:16:11 +0530

    Add files via upload

---
+++ InsertData.java
@@ -0,0 +1,30 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class InsertData {
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testdb",
+                "root",
+                "Anand@62018"
+            );
+
+            // Create Statement
+            Statement st = con.createStatement();
+
+            // Insert query
+            String query = "INSERT INTO student VALUES (1, 'Anand')";
+
+            // Execute
+            st.executeUpdate(query);
+
+            System.out.println("Data Inserted Successfully ✅");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
